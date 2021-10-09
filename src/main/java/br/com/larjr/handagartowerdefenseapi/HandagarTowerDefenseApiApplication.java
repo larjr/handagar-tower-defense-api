ede19e7dd2da3f745f41d095ea34a6a02ffd7d07
Luís Ângelo Rodrigues Jr <luisangelorjr@gmail.com>
2021-10-09 14:57:42 -0300

    Add eskeleton project

---
+++ src/main/java/br/com/larjr/handagartowerdefenseapi/HandagarTowerDefenseApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.larjr.handagartowerdefenseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandagarTowerDefenseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandagarTowerDefenseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/larjr/handagartowerdefenseapi/HandagarTowerDefenseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.larjr.handagartowerdefenseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandagarTowerDefenseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
